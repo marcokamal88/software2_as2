86e99f2d803fa13cfc218613c922803c890b0c6b
marcokamal88 <marcolkamal@gmail.com>
2023-12-30 18:51:29 +0200

    adding SMS class (now we can add any other way to send notification)

---
+++ src/main/java/com/example/demo/service/sendingSMS.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+
+public class sendingSMS implements Notifications {
+    public String sendNotification(){
+        return "SMS sending zy elfol...";
+    }
+    
+}
