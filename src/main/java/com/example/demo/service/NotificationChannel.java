3e1e13fc9268689749dd472794ef406394ab7601
Mina Daniel <90156775+minadanielferwiz@users.noreply.github.com>
2023-12-31 02:15:47 +0200

    notifi design

---
+++ src/main/java/com/example/demo/service/NotificationChannel.java
@@ -0,0 +1,5 @@
+package com.example.demo.service;
+
+public interface NotificationChnel {
+    void sendNotification(String to,NotificationTemplate template);
+}
--- src/main/java/com/example/demo/service/sendingSMS.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-
-public class sendingSMS implements Notifications {
-    public String sendNotification(){
-        return "SMS sending zy elfol...";
-    }
-    
-}
